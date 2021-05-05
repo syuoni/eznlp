6261d2de213521b6e767384dcadf5374e3e49fbb
Enwei Zhu <enwei.zhu@outlook.com>
2021-05-05 18:05:35 +0800

    focal loss and label smooth

--- eznlp/nn/modules/__init__.py
+++ eznlp/nn/modules/__init__.py
@@ -2,3 +2,4 @@
 from .aggregation import SequencePooling, SequenceAttention, SequenceGroupAggregating, ScalarMix
 from .dropout import WordDropout, LockedDropout, CombinedDropout
 from .crf import CRF
+from .loss import SoftCrossEntropyLoss, LabelSmoothCrossEntropyLoss, FocalLoss
