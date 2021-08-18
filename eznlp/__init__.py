f065f6faeaea182ac77917016a9ee5a2598ec37d
Enwei Zhu <enwei.zhu@outlook.com>
2021-08-18 10:35:30 +0800

    fix

--- eznlp/__init__.py
+++ eznlp/__init__.py
@@ -6,3 +6,9 @@ flair.device = torch.device('cpu')
 __version__ = '0.2.1'
 
 from .training.utils import auto_device
+
+import eznlp.io
+import eznlp.nn
+import eznlp.model
+import eznlp.training
+import eznlp.utils
