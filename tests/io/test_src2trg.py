06dd970d2f969930352e4e63ae7e453895a2a470
Enwei Zhu <enwei.zhu@outlook.com>
2021-10-21 17:11:26 +0800

    add src2trg datasets

--- tests/io/test_src2trg.py
+++ tests/io/test_src2trg.py
@@ -14,7 +14,18 @@ class TestSrc2TrgIO(object):
         assert len(test_data) == 1_000
         
         
-    def test_wmt14(self):
+    def test_iwslt14_en_de(self):
+        io = Src2TrgIO(tokenize_callback=None, trg_tokenize_callback=None, encoding='utf-8', case_mode='Lower', number_mode='None')
+        train_data = io.read("data/iwslt14.tokenized.de-en/train.de", "data/iwslt14.tokenized.de-en/train.en")
+        dev_data   = io.read("data/iwslt14.tokenized.de-en/valid.de", "data/iwslt14.tokenized.de-en/valid.en")
+        test_data  = io.read("data/iwslt14.tokenized.de-en/test.de", "data/iwslt14.tokenized.de-en/test.en")
+        
+        assert len(train_data) == 160_239
+        assert len(dev_data) == 7_283
+        assert len(test_data) == 6_750
+        
+        
+    def test_wmt14_en_de(self):
         io = Src2TrgIO(tokenize_callback=None, trg_tokenize_callback=None, encoding='utf-8', case_mode='Lower', number_mode='None')
         # train_data = io.read("data/wmt14/train.tok.clean.bpe.32000.de", "data/wmt14/train.tok.clean.bpe.32000.en")
         dev_data   = io.read("data/wmt14/newstest2013.tok.bpe.32000.de", "data/wmt14/newstest2013.tok.bpe.32000.en")
@@ -22,4 +33,15 @@ class TestSrc2TrgIO(object):
         
         # assert len(train_data) == 4_500_966
         assert len(dev_data) == 3_000
+        assert len(test_data) == 3_003
+        
+        
+    def test_wmt14_en_fr(self):
+        io = Src2TrgIO(tokenize_callback=None, trg_tokenize_callback=None, encoding='utf-8', case_mode='Lower', number_mode='None')
+        # train_data = io.read("data/wmt14_en_fr/train.fr", "data/wmt14_en_fr/train.en")
+        dev_data   = io.read("data/wmt14_en_fr/valid.fr", "data/wmt14_en_fr/valid.en")
+        test_data  = io.read("data/wmt14_en_fr/test.fr", "data/wmt14_en_fr/test.en")
+        
+        # assert len(train_data) == 35_762_532
+        assert len(dev_data) == 26_854
         assert len(test_data) == 3_003
