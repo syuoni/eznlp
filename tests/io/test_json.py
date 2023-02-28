4f06500d273d1e529a1c76a358da87cde8a25ecf
Enwei Zhu <enwei.zhu@outlook.com>
2023-02-28 16:24:15 +0800

    fix SciERC

--- tests/io/test_json.py
+++ tests/io/test_json.py
@@ -200,10 +200,11 @@ class TestJsonIO(object):
                          relation_key='relations', 
                          relation_type_key='type', 
                          relation_head_key='head', 
-                         relation_tail_key='tail')
-        train_data = json_io.read("data/SciERC/scierc_train.json")
-        dev_data   = json_io.read("data/SciERC/scierc_dev.json")
-        test_data  = json_io.read("data/SciERC/scierc_test.json")
+                         relation_tail_key='tail', 
+                         encoding='utf-8')
+        train_data = json_io.read("data/SciERC/train.json")
+        dev_data   = json_io.read("data/SciERC/dev.json")
+        test_data  = json_io.read("data/SciERC/test.json")
         
         assert len(train_data) == 1_861
         assert sum(len(ex['chunks']) for ex in train_data) == 5_598
@@ -217,6 +218,25 @@ class TestJsonIO(object):
         
         assert max(detect_overlapping_level(ex['chunks']) for ex in train_data+dev_data+test_data) == NESTED
         assert all(filter_clashed_by_priority(ex['chunks'], allow_level=NESTED) == ex['chunks'] for ex in train_data+dev_data+test_data)
+        
+        
+    def test_SciERC_eberts2020ecai(self):
+        json_io = JsonIO(text_key='tokens', 
+                         chunk_key='entities', 
+                         chunk_type_key='type', 
+                         chunk_start_key='start', 
+                         chunk_end_key='end', 
+                         relation_key='relations', 
+                         relation_type_key='type', 
+                         relation_head_key='head', 
+                         relation_tail_key='tail')
+        train_data = json_io.read("data/SciERC-eberts2020ecai/scierc_train.json")
+        dev_data   = json_io.read("data/SciERC-eberts2020ecai/scierc_dev.json")
+        test_data  = json_io.read("data/SciERC-eberts2020ecai/scierc_test.json")
+        
+        assert len(train_data) == 1_861
+        assert len(dev_data) == 275
+        assert len(test_data) == 551
         
         
     def test_ADE(self):
